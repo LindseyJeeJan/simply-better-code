ad0de01969a7eb38614ee4fbab6dabf60196ccd2
Lindsey JJ <ljjlauria@mac.com>
2023-02-22 21:04:21 -0800

    webpack update

--- src/index.tsx
+++ src/index.tsx
@@ -40,9 +40,7 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <RouterProvider router={router} />,
-
   // <Provider store={store}>
-
+  <RouterProvider router={router} />,
   // </Provider>,
 );
