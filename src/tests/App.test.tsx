06a26d715c229659dd0fec4ae922e89e40e3e2fa
Lindsey JJ <ljjlauria@mac.com>
2025-11-08 15:40:41 -0800

    test: mock react-router-dom to avoid ESM resolver issues in Jest

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
+
+// Mock react-router-dom for the test environment to avoid resolver issues
+// introduced by newer ESM-only builds of react-router-dom when running
+// Jest in this project setup.
+jest.mock('react-router-dom', () => ({
+  NavLink: (props: any) => <a {...props}>{props.children}</a>,
+  BrowserRouter: ({ children }: any) => <div>{children}</div>,
+  Link: (props: any) => <a {...props}>{props.children}</a>,
+  Routes: ({ children }: any) => <div>{children}</div>,
+  Route: ({ children }: any) => <div>{children}</div>,
+}));
+
 import { render } from '@testing-library/react';
 import App from 'components/App';
 
