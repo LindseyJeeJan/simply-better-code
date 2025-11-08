2c8f06ce23dbca3e965073f25829cc31f207b41e
Lindsey JJ <ljjlauria@mac.com>
2025-11-08 15:45:42 -0800

    test: use real MemoryRouter (remove accidental mock)

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,16 +1,4 @@
 import React from 'react';
-
-// Mock react-router-dom for the test environment to avoid resolver issues
-// introduced by newer ESM-only builds of react-router-dom when running
-// Jest in this project setup.
-jest.mock('react-router-dom', () => ({
-  NavLink: (props: any) => <a {...props}>{props.children}</a>,
-  BrowserRouter: ({ children }: any) => <div>{children}</div>,
-  Link: (props: any) => <a {...props}>{props.children}</a>,
-  Routes: ({ children }: any) => <div>{children}</div>,
-  Route: ({ children }: any) => <div>{children}</div>,
-}));
-
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from 'components/App';
